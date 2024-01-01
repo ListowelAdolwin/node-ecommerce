d1ffc01816357fdf34c098d4aec488ea0d6f7c3b
ListowelAdolwin <listoweladolwin@gmail.com>
2024-01-01 23:44:10 +0000

    Updated db to MySQL

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize')
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'node_ecommerce',
-    database: 'node_ecommerce',
-    password: 'node_ecommerce'
-});
+const sequelize = new Sequelize('node_ecommerce', 'node_ecommerce', 'node_ecommerce', {
+    dialect: 'mysql',
+    host: 'localhost'
+})
 
-module.exports = pool.promise();
+module.exports = sequelize
